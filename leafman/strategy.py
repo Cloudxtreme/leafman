433861df0a7ca52e41219d65e8376adfa61a3bd4
Eugene Eeo <xe0jun@hotmail.com>
2014-10-20 18:40:19 +0800

    lint code using pep8 and pylint

--- leafman/strategy.py
+++ leafman/strategy.py
@@ -6,35 +6,35 @@
 """
 
 
-def jaccard(d1, d2):
+def jaccard(query, value):
     """
     Performs the jaccard index on two given iterables,
     returning 0 if one of them is empty.
 
-    :param d1: The first iterable.
-    :param d2: Another iterable.
+    :param query: The first iterable.
+    :param value: Another iterable.
     """
-    d1, d2 = set(d1), set(d2)
-    if not (d1 or d2):
+    query, value = set(query), set(value)
+    if not (query or value):
         return 0.0
-    inter = len(d1 & d2)
-    union = len(d1 | d2)
+    inter = len(query & value)
+    union = len(query | value)
     return inter / float(union)
 
 
-def relevance(d1, d2):
+def relevance(query, value):
     """
     Performs relevance index on two iterables.
 
-    :param d1: The first iterable.
-    :param d2: Another iterable.
+    :param query: The first iterable.
+    :param value: Another iterable.
     """
-    length = len(d2)
-    if not length or len(d1) > length:
+    length = len(value)
+    if not length or len(query) > length:
         return 0.0
-    d2, matches = iter(d2), 0
-    for char in d1:
-        for item in d2:
+    value, matches = iter(value), 0
+    for char in query:
+        for item in value:
             if item == char:
                 matches += 1
                 break
