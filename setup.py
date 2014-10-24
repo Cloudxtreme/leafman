8f3556e09592ced362a40da437c87a0d04d2315f
Eugene Eeo <xe0jun@hotmail.com>
2014-10-24 16:10:16 +0800

    better setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,19 @@ setup(
     name='leafman',
     version='0.1.4',
     description='simple and fast library for computing suggestions',
+    license='MIT',
     author='Eugene Eeo',
     author_email='packwolf58@gmail.com',
     url='https://github.com/eugene-eeo/leafman',
     packages=['leafman'],
+
+    platforms='any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ]
 )
