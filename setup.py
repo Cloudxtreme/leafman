d30c7dcdb0bd361b7a768eed066b7b315d442b7c
Eugene Eeo <xe0jun@hotmail.com>
2014-11-01 11:53:02 +0800

    bump version to 2.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='leafman',
-    version='0.2.0',
+    version='0.2.1',
     description='simple and fast library for computing suggestions',
     license='MIT',
     author='Eugene Eeo',
