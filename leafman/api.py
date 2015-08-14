a5ca1d728756e3001fa4039a13d5543ab8822baf
Eugene Eeo <packwolf58@gmail.com>
2015-08-14 23:28:48 +0800

    Actually implement suggest function

--- leafman/api.py
+++ leafman/api.py
@@ -2,11 +2,8 @@ from .trie import Trie
 from .scoring import jaccard
 
 
-def fmap(funcs, item):
-    for fn in funcs:
-        item = fn(item)
-    return item
-
-
-def suggest(query, words, score=jaccard, threshold=0.3):
-    pass
+def suggest(query, words, metric=jaccard, threshold=0.3):
+    for item in Trie(words).get(query):
+        score = metric(query, item)
+        if score >= threshold:
+            yield item, score
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='leafman',
-    version='0.3.0',
+    version='0.4.0',
     description='simple and fast library for computing suggestions',
     license='MIT',
     author='Eugene Eeo',
