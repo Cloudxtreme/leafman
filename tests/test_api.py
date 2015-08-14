1fc5edb810aed0560fcf2b7d2cd1e1641140feb5
Eugene Eeo <packwolf58@gmail.com>
2015-08-14 23:50:25 +0800

    Test new jaccard function

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,17 @@
 import pytest
-from leafman.api import suggest
+from leafman.api import suggest, jaccard
 
 
 def test_suggest():
     suggested = suggest('abc', ['abc', 'abcdef'])
     expected = [('abc', 1.0), ('abcdef', 0.5)]
     assert list(suggested) == expected
+
+
+def test_jaccard():
+    u = jaccard('a')
+    assert u('') == 0.0
+    assert u('b') == 0.0
+    assert u('a') == 1.0
+    assert u('ab') == 0.5
+    assert u('abcd') == 0.25
